3de278b1c579598d12ff7d0e7acaa014656167f9
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-09-22 21:01:38 -0500

    updated ref and resolvers for link queries

--- src/index.js
+++ src/index.js
@@ -2,14 +2,34 @@ const { GraphQLServer } = require("graphql-yoga");
 
 const typeDefs = `
 type Query {
-    info: String! 
+    info: String!
+    feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `;
+
+let links =[{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
 
 const resolvers = {
   Query: {
     info: () => `This is the API of HackerNews clone`,
+    feed: () => links,
   },
+  
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
+  }
 };
 
 const server = new GraphQLServer({
