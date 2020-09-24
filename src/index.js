fda5f71481672eb3910d885f8fdcb5ef15d9f575
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-09-23 21:44:45 -0500

    added link mutation

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-const typeDefs = `
-type Query {
-    info: String!
-    feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`;
-
 let links =[{
   id: 'link-0',
   url: 'www.howtographql.com',
   description: 'Fullstack tutorial for GraphQL'
 }]
 
+let idCounter = links.length;
 const resolvers = {
   Query: {
     info: () => `This is the API of HackerNews clone`,
     feed: () => links,
   },
-  
+
+  Mutation: {
+    post: (parent, arg) => {
+      const link = {
+        id: `link-${idCounter++}`,
+        description: arg.description,
+        url: arg.url,
+      };
+      links.push(link);
+      return link;
+    },
+  },
+
   Link: {
     id: (parent) => parent.id,
     description: (parent) => parent.description,
     url: (parent) => parent.url,
-  }
+  },
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers,
 });
 
