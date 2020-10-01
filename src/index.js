a5902163e7a52c4cac9b5f19bc124f2210ed86c1
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-10-01 01:36:03 -0500

    updated resolvers for link and user/added post mutation

--- src/index.js
+++ src/index.js
@@ -1,41 +1,29 @@
 const { GraphQLServer } = require("graphql-yoga");
 const { PrismaClient } = require("@prisma/client");
 
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+
 const prisma = new PrismaClient();
 
 const resolvers = {
-  Query: {
-    info: () => `This is the API of HackerNews clone`,
-    feed: async (parent, args, context, info) => {
-      return context.prisma.link.findMany();
-    },
-  },
-
-  Mutation: {
-    post: (parent, args, context) => {
-      const newLink = context.prisma.link.create({
-        data: {
-          url:   args.url,
-          description: args.description,
-        },
-      });
-      return newLink;
-    },
-  },
-
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
-  },
+  Query,
+  Mutation,
+  User,
+  Link,
 };
 
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers,
-  context: {
-    prisma,
+  context: (request) => {
+    return {
+      ...request,
+      prisma,
+    };;
   },
 });
 
