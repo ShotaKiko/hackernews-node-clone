26db126ad0f4ef73d752de9e8440346f554f01fc
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-10-06 18:02:47 -0500

    added vote resolver functions

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -50,8 +50,39 @@ function post(parent, args, context, info) {
   });
 }
 
+async function vote(parent, args, context, info) {
+  // 1
+  const userId = getUserId(context);
+
+  // 2
+  const vote = await context.prisma.vote.findOne({
+    where: {
+      linkId_userId: {
+        linkId: Number(args.linkId),
+        userId: userId,
+      },
+    },
+  });
+
+  if (Boolean(vote)) {
+    throw new Error(`Already voted for link: ${args.linkId}`);
+  }
+
+  // 3
+  const newVote = context.prisma.vote.create({
+    data: {
+      user: { connect: { id: userId } },
+      link: { connect: { id: Number(args.linkId) } },
+    },
+  });
+  context.pubsub.publish("NEW_VOTE", newVote);
+
+  return newVote;
+}
+
 module.exports = {
   signup,
   login,
   post,
+  vote,
 };
